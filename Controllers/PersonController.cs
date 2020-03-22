958d9e4b187e69e60848f9c46b5d52b2ac160936
Ethan <olpin3@gmail.com>
2020-03-22 16:03:28 -0700

    Add project files.

---
+++ Controllers/PersonController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Test.Models;
+
+namespace Test.Controllers
+{
+    [Route("Person")]
+    [Produces("application/json")]
+    public class PersonController : Controller
+    {
+         public static List<Person> people = new List<Person> { new Person { Name = "dave", Birthday = new DateTime(2000, 6, 1, 0, 0, 0), ImageURL = "" } };
+         [HttpGet]
+         public List<Person> GetPersons()
+        {
+            return people;
+        }
+
+        [HttpPost]
+        public IActionResult AddPerson([FromBody]Person person)
+        {
+            people.Add(person);
+            return Ok(new
+            { 
+                success = true,
+                returncode = "200"
+            });
+        }
+
+    }
+}
---
+++ Models/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Test.Models
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string ImageURL { get; set; }
+        public DateTime Birthday { get; set; }
+
+    }
+}
