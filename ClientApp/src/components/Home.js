4b7f91e87ab0fc67338e4f21803af6317ec79308
Ethan <olpin3@gmail.com>
2020-03-22 22:45:00 -0700

    Created search component. Added some styling

--- ClientApp/src/components/Home.js
+++ ClientApp/src/components/Home.js
@@ -1,4 +1,4 @@
-import React, { Component, useState, useEffect } from 'react';
+import React, { useState } from 'react';
 
 const Home = () => {
 
@@ -7,21 +7,13 @@ const Home = () => {
     const [lastName, setLastName] = useState();
     const [birthday, setBirthday] = useState();
     const [imageURL, setImageURL] = useState();
-    const [search, setSearch] = useState("");
-    const [searchResult, setSearchResult] = useState([]);
-
-    const getPersons = async () => {
-        const response = await fetch('person?search=' + search)
-        const data = await response.json();
-        setSearchResult(data)
-    }
 
     const addPerson = async e => {
         e.preventDefault()
         const body = { firstName, lastName, birthday, imageURL }
-        const response = await fetch('person', { method: "POST", body: JSON.stringify(body), headers: { "content-type": "application/json"}})
+        const response = await fetch('person', { method: "POST", body: JSON.stringify(body), headers: { "content-type": "application/json" } })
     }
-    
+
     return (
         <>
             <form onSubmit={addPerson}>
@@ -38,10 +30,6 @@ const Home = () => {
                 <input type="text" onChange={e => setImageURL(e.target.value)} />
                 <button>Submit</button>
             </form>
-            <input type="text" onChange={e => setSearch(e.target.value)}/>
-            <button onClick={getPersons}>Search</button>
-            <ul>{searchResult.map(p => <li>{p.firstName}</li>)}</ul>
-
         </>
     )
 }
---
+++ ClientApp/src/components/Search.js
@@ -0,0 +1,42 @@
+﻿import React, { useState } from 'react';
+import moment from 'moment';
+const Search = () => {
+
+    const [search, setSearch] = useState("");
+    const [searchResult, setSearchResult] = useState([]);
+
+    const getPersons = async () => {
+        const response = await fetch('person?search=' + search)
+        const data = await response.json()
+        setSearchResult(data)
+    }
+
+    return (
+        <>
+            <input type="text" onChange={e => setSearch(e.target.value)} className="form-control" style={{ display: "inline", width: "400px" }}/>
+            <button onClick={getPersons} className="btn btn-primary" style={{ display: "inline" }}>Search</button>
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th></th>
+                        <th>Image</th>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Age</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {searchResult.map((p, i) =><tr>
+                            <th>{i+1}</th>
+                            <td><img src={p.imageURL} /></td>
+                            <td>{p.firstName}</td>
+                            <td>{p.lastName}</td>
+                        <td>{moment().diff(moment(p.birthday), "year")}</td>
+                    </tr>)}
+                </tbody>
+            </table>
+        </>
+    )
+}
+
+export default Search
